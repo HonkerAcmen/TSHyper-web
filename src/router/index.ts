eac49f50ab9fe42991f2bac1d455a65f9295a73c
Mark Yang <haker0x04@163.com>
2024-05-26 20:23:03 +0800

    完成路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,14 +7,42 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      component: Layout
+      component: Layout,
+      beforeEnter(to, from ,next){
+        if (window.localStorage.getItem('token')){
+          next('/home')
+        }else{
+          next()
+        }
+      }
     },
     {
       path: '/home',
       component: Manage
-    }
+    },
+    
 
   ]
+})
+
+// 路由守卫
+router.beforeEach((to, from, next)=>{
+  let isValid = false
+  if (window.localStorage.getItem('token') != null){
+    isValid = true
+  }else{
+    isValid = false
+  }
+
+  // 如果token不为空或地址为layout时 下一个 否则一直是layout.
+  // 该表达式从左往右计算，假设isValid为false，那么它会判断右式是否为真.
+  // 因为layout的下一个地址是它自己，所以会成为一个条件锁。
+  // 当isValid为真，那么后面的条件也不会去判断了.
+  if (isValid || to.path === '/'){
+    next()
+  }else{
+    next('/')
+  }
 })
 
 export default router
