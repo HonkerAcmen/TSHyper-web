b7ac2b7f7ece09c077c07b66dec5a226e414d17c
Mark Yang <haker0x04@163.com>
2024-05-27 12:43:31 +0800

    完成侧边栏、头栏的初次开发

--- src/views/manage/index.vue
+++ src/views/manage/index.vue
@@ -13,7 +13,27 @@
 
     <!-- 侧边栏 -->
     <div class="side">
-
+        <div class="firstpass">
+            <div class="p1">
+                <p><el-icon><HomeFilled /></el-icon><span><a href="/">主页</a></span></p>
+            </div>
+            <div class="p2">
+                <p><el-icon><Failed /></el-icon><span><a href="/">草稿箱</a></span></p>
+            </div>
+            <div class="p3">
+                <p><el-icon><Comment /></el-icon><span><a href="/">我的消息</a></span></p>
+            </div>
+        </div>
+        <div class="pass"></div>
+        <div class="secondpass">
+            <p><el-icon><UserFilled /></el-icon><span>{{ user }}的组织</span></p>
+        </div>
+        <div class="pass"></div>
+        <div class="thirdpass">
+            <div class="p1">
+            <p><el-icon><Plus /></el-icon><span><a href="/">新建团队</a></span></p>
+            </div>
+        </div>
     </div>
 
     <!-- 主栏 -->
